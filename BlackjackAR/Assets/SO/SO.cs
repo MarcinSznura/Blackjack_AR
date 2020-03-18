13d678172d7b0617fe43135b99c48c0c99ca6cf6
Mar-zin <marcinanyway@gmail.com>
2020-03-18 13:45:25 +0100

    Vuforia database for cards

---
+++ BlackjackAR/Assets/SO/SO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string prefabName;
+
+    public int cardValue;
+    public GameObject object3D;
+
+
+
+}

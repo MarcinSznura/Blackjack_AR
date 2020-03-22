a099cdf7edc89c191296558f554110badc239e8e
Marcin Sznura <marcinanyway@gmail.com>
2020-03-22 21:22:47 +0100

    Detecting multiple occurrences of the same Image

---
+++ Blackjack_AR/BlackjackAR/Assets/DefaultTrackableEventHandlerForCards.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Vuforia
+{
+    public class DefaultTrackableEventHandlerForCards : MonoBehaviour, ITrackableEventHandler
+    {
+        public int id;
+        public bool tracked;
+
+        [Header("Tracking time info")]
+        public float presentTime;
+        [SerializeField] Coroutine timer;
+        
+
+        #region PRIVATE_MEMBER_VARIABLES
+
+        private TrackableBehaviour mTrackableBehaviour;
+
+        #endregion // PRIVATE_MEMBER_VARIABLES
+
+        #region UNTIY_MONOBEHAVIOUR_METHODS
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this
+                );
+            }
+        }
+        #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+        #region PUBLIC_METHODS
+
+        public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+
+else
+            {
+                OnTrackingLost();
+            }
+        }
+        #endregion // PUBLIC_METHODS
+
+        #region PRIVATE_METHODS
+
+        IEnumerator CountTime()
+        {
+            while (true)
+            {
+                presentTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        private void OnTrackingFound()
+        {
+            tracked = true;
+            timer = StartCoroutine(CountTime());
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+            Debug.Log(" Trackable " +mTrackableBehaviour.TrackableName +" found");
+            /*
+            if (cardName == "Joker" && CanIRead())
+            {
+                FindObjectOfType<GameMaster>().SetJokerAppeared(true);
+            }
+            */
+        }
+
+
+        private void OnTrackingLost()
+        {
+            tracked = false;
+            presentTime = 0;
+            if (timer != null) StopCoroutine(timer);
+            Renderer[] rendererComponents = GetComponentsInChildren <Renderer> (true);
+            Collider[] colliderComponents = GetComponentsInChildren < Collider > (true);
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+            Debug.Log(" Trackable " +mTrackableBehaviour.TrackableName
+                + " lost ");
+        }
+
+        #endregion // PRIVATE_METHODS
+    }
+}
---
+++ Blackjack_AR/BlackjackAR/Assets/Validator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Validator : MonoBehaviour
+{
+    [Header("Children")]
+    [SerializeField] DefaultTrackableEventHandlerForCards[] children;
+    [SerializeField] float timeToCountCard = 1f;
+    [SerializeField] float minDistanceBetweenCards;
+
+    [Header("Cards parameters")]
+    [SerializeField] int cardValue;
+    [SerializeField] int countedCards;
+
+    private GameMaster gameMaster;
+
+    private void Awake()
+    {
+        children = GetComponentsInChildren<DefaultTrackableEventHandlerForCards>();
+        gameMaster = FindObjectOfType<GameMaster>();
+    }
+
+    void Update()
+    {
+        if (CanIRead())
+        {
+            if (NumberOfMyChildrenInfrontOfCamera() > countedCards)
+            {
+                countedCards++;
+                gameMaster.IncreasePlayerScore(cardValue);
+                gameMaster.playerHand.Add(cardValue);
+                Debug.Log("I counted card: " + cardValue.ToString());
+            }
+        }
+
+        foreach (DefaultTrackableEventHandlerForCards child in children)
+        {
+            foreach (DefaultTrackableEventHandlerForCards child2 in children)
+            {
+                if (child.id != child2.id)
+                {
+                    if (Vector3.Distance(child.transform.position, child2.transform.position) < minDistanceBetweenCards
+                        && child.tracked && child2.tracked)
+                    {
+                        child.presentTime = 0;
+                        print(Vector3.Distance(child.transform.position, child2.transform.position));
+                        //Debug.LogError("TOO FUCKING CLOSE!");
+                    }
+                    else
+                    {
+                       
+                    }
+                }
+            }
+        }
+    }
+
+    private int NumberOfMyChildrenInfrontOfCamera()
+    {
+        int cardNumber = 0;
+        foreach (DefaultTrackableEventHandlerForCards child in children)
+        {
+            if (child.presentTime > timeToCountCard)
+            {
+                cardNumber += 1;
+            }
+        }
+        return cardNumber;
+    }
+
+    private bool CanIRead()
+    {
+        if (gameMaster.GetCurrentStage() == 4
+            || gameMaster.GetCurrentStage() == 3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void RestartCountedCards()
+    {
+        countedCards = 0;
+    }
+
+}
